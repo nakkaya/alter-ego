fef813624a4b2b1d102bbb0477c01ce476c60926
Nurullah Akkaya <nurullah@nakkaya.com>
2010-06-13 02:55:52 +0300

    fix README, edit dependencies.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject alter-ego "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+(defproject alter-ego "0.0.1-SNAPSHOT"
+  :description "A Behavior Tree Implementation."
+  :dependencies [[org.clojure/clojure "1.1.0"]
 		 [org.clojars.nakkaya/miglayout "3.7.3.1"]]
-  :main alter_ego.gui.editor)
+  :main alter_ego.gui.editor
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
