eefa1ef110c60dbe4881e3c090d058cadf15cc1c
Nurullah Akkaya <nurullah@nakkaya.com>
2010-06-13 02:39:02 +0300

    implement loading from a saved tree.

--- src/alter_ego/core.clj
+++ src/alter_ego/core.clj
@@ -1,1 +1,33 @@
-(ns alter-ego.core)
+(ns alter-ego.core
+  (:refer-clojure :exclude [sequence])
+  (:use [alter-ego.node-types] :reload-all)
+  (:use [alter-ego.composite] :reload-all)
+  (:use [alter-ego.decorator] :reload-all))
+
+(defn- node [n blackboard]
+  (let [{type :type func :action} n]
+    (cond (= type :selector) (selector [])
+	  (= type :sequence) (sequence [])
+	  (= type :action) (action (symbol func) blackboard)
+	  (= type :until-fail) (until-fail nil))))
+
+(defn- add-child [p c]
+  (let [{children :children} p]
+    (if (isa? (until-fail nil) p)
+      (until-fail c)
+      (assoc p :children (conj children c)))))
+
+(defn load-tree 
+  ([file]
+     (load-tree file (ref {})))
+  ([file blackboard]
+     (let [tree (read-string (slurp file))] 
+       (load-tree (first tree) (rest tree) blackboard)))
+  ([parent children blackboard]
+     (reduce (fn[h v] 
+	       (if (> (count v) 1)
+		 (let [p (node (first v) blackboard)
+		       c (rest v)] 
+		   (add-child h (load-tree p c blackboard)))
+		 (add-child h (node (first v) blackboard)))) 
+	     parent children)))
--- test/alter_ego/core_test.clj
+++ test/alter_ego/core_test.clj
@@ -1,6 +1,36 @@
 (ns alter-ego.core-test
+  (:refer-clojure :exclude [sequence])
+  (:use [alter-ego.composite] :reload-all)
+  (:use [alter-ego.decorator] :reload-all)
   (:use [alter-ego.core] :reload-all)
   (:use [clojure.test]))
 
-;; (deftest replace-me ;; FIXME: write
-;;   (is false))
+(defmacro with-private-fns [[ns fns] & tests]
+  "Refers private fns from ns and runs tests in context."
+  `(let ~(reduce #(conj %1 %2 `(ns-resolve '~ns '~%2)) [] fns)
+     ~@tests))
+
+(def sample-tree
+     [{:type :selector, :name "Root"}
+      [{:type :sequence, :name "Open Door"}
+       [{:type :action, :name "Door Open?", :action 'door-open?}]
+       [{:type :action, :name "Move", :action 'move}]]
+      [{:type :sequence, :name "Closed Door"}
+       [{:type :action, :name "Move", :action 'move}]
+       [{:type :action, :name "Open Door", :action 'open}]
+       [{:type :until-fail, :name "Until Fail"}
+	[{:type :action, :name "Move", :action 'move}]]]])
+
+(with-private-fns [alter-ego.core [node]]
+  (deftest load-test
+    (let [blackboard (ref {})
+	  tree (load-tree (node (first sample-tree) blackboard)
+			  (rest sample-tree) blackboard)]
+      (is (= 'door-open? 
+	     (:symbol (first (:children (first (:children tree)))))))
+      (is (= 'move 
+	     (:symbol (second (:children (first (:children tree)))))))
+      (is (= 'move 
+	     (:symbol (first (:children (second (:children tree)))))))
+      (is (= 'open
+	     (:symbol (second (:children (second (:children tree))))))))))
