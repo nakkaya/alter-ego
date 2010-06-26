28c84bc84ae30e5aaf5b56d0bc9c099c0dab2155
Nurullah Akkaya <nurullah@nakkaya.com>
2010-06-26 19:34:33 +0300

    bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject alter-ego "0.0.1-SNAPSHOT"
+(defproject alter-ego "0.0.2-SNAPSHOT"
   :description "A reactive AI library based on the concept of behavior trees."
   :dependencies [[org.clojure/clojure "1.1.0"]
 		 [org.clojars.nakkaya/miglayout "3.7.3.1"]]
