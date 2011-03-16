af281a4357dbe2844d45365ea9ae609cadb6183d
Nurullah Akkaya <nurullah@nakkaya.com>
2011-03-16 16:01:33 +0200

    fix main in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject alter-ego "0.0.3-SNAPSHOT"
   :description "A reactive AI library based on the concept of behavior trees."
   :dependencies [[org.clojure/clojure "1.2.0"]]
-  :main alter_ego.gui.editor
+  :main alter-ego.gui.editor
   :dev-dependencies [[lein-clojars "0.6.0"]
 		     [autodoc "0.7.1"]])
