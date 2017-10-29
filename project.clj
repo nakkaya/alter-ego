2d1aa3d364d6fddb4c04a0fa055335c37dff6efe
Nurullah Akkaya <nurullah@nakkaya.com>
2017-10-29 23:44:32 +0300

    bump clojure version

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject alter-ego "0.0.5-SNAPSHOT"
+(defproject alter-ego "0.0.6"
   :description "A reactive AI library based on the concept of behavior trees."
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]])
