50320ee5a4e381f717c2c575fe8f8f64de72e6b9
Nurullah Akkaya <nurullah@nakkaya.com>
2017-11-02 20:47:57 +0200

    deploy clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject alter-ego "0.0.6"
+(defproject alter-ego "0.0.7"
   :description "A reactive AI library based on the concept of behavior trees."
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :repositories [["releases" {:url "https://clojars.org/repo"
